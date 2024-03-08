91c30df24260ef7722eeb85e9a8bf2f579993db8
Erick Araujo <erickequipe7@gmail.com>
2024-03-08 12:42:57 -0300

    Added life system in UI

--- Assets/_Project/Scripts/Runtime/Systems/Hit/Status.cs
+++ Assets/_Project/Scripts/Runtime/Systems/Hit/Status.cs
@@ -13,6 +13,8 @@ public class Status : MonoBehaviour
 
     public event Action OnDie;
 
+    public event Action OnPlayerHit;
+
     [SerializeField] private GameObject hitPrefab;
 
     public int maxLife;
@@ -22,6 +24,11 @@ public class Status : MonoBehaviour
         //GameObject temp = Instantiate(hitPrefab, transform.position, Quaternion.identity);
         //Destroy(temp, 0.5f);
         maxLife -= value;
+
+        if (statusTag.Equals(StatusTag.Player))
+        {
+            OnPlayerHit?.Invoke();
+        }
 
         if (maxLife <= 0)
         {
