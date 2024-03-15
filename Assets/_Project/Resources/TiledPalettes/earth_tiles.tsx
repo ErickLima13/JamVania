516b70c1e909a40ed28e234c359c0c543cc7a5dd
thegreatgarzet <52185914+thegreatgarzet@users.noreply.github.com>
2024-03-15 16:51:07 -0300

    Added changes to map

---
+++ Assets/_Project/Resources/TiledPalettes/earth_tiles.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="earth_tiles" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <image source="../../Art/Textures/earth_tiles.png" width="128" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
